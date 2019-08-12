e205f79fe0cd5f116150414c17cfd3ceadf47105
Yevgeniy Ivanov <anigeka@gmail.com>
2019-08-12 18:49:02 -0400

    timer added

--- program.js
+++ program.js
@@ -1,30 +1,32 @@
 var canvas;
 var canvasContext;
-var x = 200;
-var y = 200;
 var snackX = 0;
 var snackY = 0;
 var turningPoints = [];
-var traveledLinks = [];
 var snackEaten = false;
-var changedDirection = false;
-var snake = [
-    { x: 280, y: 200, dx: 20, dy: 0 },
-    { x: 260, y: 200, dx: 20, dy: 0 },
-    { x: 240, y: 200, dx: 20, dy: 0 },
-    { x: 220, y: 200, dx: 20, dy: 0 },
-    { x: 200, y: 200, dx: 20, dy: 0 },
-]
+var snake = [];
 var head;
 var tail;
+var ableToChangeDirection = new Date(Date.now() + 200);
 
 window.onload = function () {
     // main canvas
     canvas = document.getElementById("gameCanvas");
     canvasContext = canvas.getContext("2d");
 
+    createSnake();
     createSnack();
     setInterval(gameRun, 500);
+}
+
+function createSnake() {
+    snake = [
+        { x: canvas.width / 2 + 40, y: canvas.height / 2, dx: 20, dy: 0 },
+        { x: canvas.width / 2 + 20, y: canvas.height / 2, dx: 20, dy: 0 },
+        { x: canvas.width / 2, y: canvas.height / 2, dx: 20, dy: 0 },
+        { x: canvas.width / 2 - 20, y: canvas.height / 2, dx: 20, dy: 0 },
+        { x: canvas.width / 2 - 40, y: canvas.height / 2, dx: 20, dy: 0 },
+    ]
 }
 
 function gameRun() {
@@ -70,22 +72,20 @@ function checkForLoss() {
 function moveSnake() {
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0, 0, canvas.clientWidth, canvas.height);
-
-    snake.forEach(link => {
-        for (let i = 0; i < turningPoints.length; i++) {
-            turn = turningPoints[i];
-            // make it so links that have already traveled that turn
-            // cannot be turned there again
+    for (let i = 0; i < snake.length; i++) {
+        var link = snake[i];
+        for (let j = 0; j < turningPoints.length; j++) {
+            var turn = turningPoints[j];
             if (link.x === turn.x && link.y === turn.y) {
+
                 link.dx = turn.dx;
                 link.dy = turn.dy;
 
-                // once the tail touches the turning point, it needs to 
-                // be removed
+                // once the tail touches the turning point, the point
+                // needs to be removed
                 if (turn.x === tail.x && turn.y === tail.y) {
                     turningPoints.shift();
                 }
-                traveledLinks[i].push(link);
             }
         }
 
@@ -93,7 +93,7 @@ function moveSnake() {
         link.y += link.dy;
         canvasContext.fillStyle = 'red';
         canvasContext.fillRect(link.x, link.y, 19, 19);
-    });
+    }
 }
 
 function maintainSnack() {
@@ -124,22 +124,35 @@ function checkSpace(xs, ys) {
 }
 
 function checkX(xs) {
-    var randomX = Math.floor(Math.random() * Math.floor(canvas.width));
-    if (xs.includes(randomX) || randomX % 20 != 0) { return checkX(xs); }
+    let min = 1;
+    let max = canvas.width / 20;
+    var randomX = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomX *= 20;
+    if (xs.includes(randomX)) { return checkX(xs); }
     return randomX;
 }
 
 function checkY(ys) {
-    var randomY = Math.floor(Math.random() * Math.floor(canvas.height));
-    if (ys.includes(randomY) || randomY % 20 != 0) { return checkX(ys); }
+    let min = 1;
+    let max = canvas.height / 20;
+    var randomY = Math.floor(Math.random() * (max - min + 1)) + min;
+    randomY *= 20;
+    if (ys.includes(randomY)) { return checkY(ys); }
     return randomY;
 }
 
 function gameOver() {
-    alert('Game Over');
+    console.log('game over');
 }
 
 window.addEventListener("keydown", (e) => {
+    // This prevents button-mashing so the code can operate
+    if (Date.now() < ableToChangeDirection) { return; }
+    ableToChangeDirection = new Date(Date.now() + 300);
+    // The cases create a turning point on the grid by which
+    // all the snake pieces have to abide. So if the point says
+    // that the snake went in the positive x direction, every
+    // link following the head will have its direction changed likewise
     switch (e.keyCode) {
         case 65:
         case 37:
@@ -147,7 +160,6 @@ window.addEventListener("keydown", (e) => {
                 head.dx = -20;
                 head.dy = 0;
                 turningPoints.push({ x: head.x, y: head.y, dx: head.dx, dy: head.dy })
-                traveledLinks.push([]);
             }
             break;
         case 83:
@@ -156,7 +168,6 @@ window.addEventListener("keydown", (e) => {
                 head.dy = 20;
                 head.dx = 0;
                 turningPoints.push({ x: head.x, y: head.y, dx: head.dx, dy: head.dy })
-                traveledLinks.push([]);
             }
             break;
         case 87:
@@ -165,7 +176,6 @@ window.addEventListener("keydown", (e) => {
                 head.dy = -20;
                 head.dx = 0;
                 turningPoints.push({ x: head.x, y: head.y, dx: head.dx, dy: head.dy })
-                traveledLinks.push([]);
             }
             break;
         case 68:
@@ -174,7 +184,6 @@ window.addEventListener("keydown", (e) => {
                 head.dx = 20;
                 head.dy = 0;
                 turningPoints.push({ x: head.x, y: head.y, dx: head.dx, dy: head.dy })
-                traveledLinks.push([]);
             }
             break;
     };
